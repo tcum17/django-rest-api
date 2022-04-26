8bf18102e0bbe99e43bffeb7372756a3ce424162
Tyler <tscummi@ilstu.edu>
2022-04-25 19:01:51 -0500

    Adding files to repo

---
+++ main/api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from .serializer import BookModelSerializer
+from main.models import Book
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
